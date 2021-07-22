71e9db69658db14d0e7095950ab0c71657ec1100
Thomas Braccia <bracciata@gmail.com>
2021-07-22 14:09:53 -0500

    Progress bar now loading and working for all three locations.

--- screens/capacity/CapacityScreen.jsx
+++ screens/capacity/CapacityScreen.jsx
@@ -1,20 +1,37 @@
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
 import * as Progress from 'react-native-progress';
-const gym = require('./gymTimes.json');
+const gym = require('./data/gymTimes.json');
+const caf = require('./data/cafTimes.json');
+const lot = require('./data/lotTimes.json');
 
 function getProgress(location) {
   var time = new Date();
-  var dataset = gym;
+  let dataset = gym;
   switch (location) {
     case 'gym':
       dataset = gym;
+      break;
+    case 'lot':
+      dataset = lot;
+      break;
+    case 'caf':
+      console.log('caf');
+      dataset = caf;
+      break;
+    default:
+      console.log('Please pass a valid location')
   }
+
   if (time.getDay() >= 1 && time.getDay() < 6) {
     // It is a weekday
-    return ((dataset.peopleAtTimes[0].weekdays[time.getHours()][time.getHours().toString() + "00"]) / dataset.maxCapacity);
+    let capacity = ((dataset.peopleAtTimes[0].weekdays[time.getHours()][time.getHours().toString() + "00"]) / dataset.maxCapacity);
+    console.log(`${location} at ${capacity} of capacity`)
+    return capacity;
   } else {
-    return ((dataset.peopleAtTimes[0].weekends[time.getHours()][time.getHours().toString() + "00"]) / dataset.maxCapacity);
+    let capacity = ((dataset.peopleAtTimes[0].weekends[time.getHours()][time.getHours().toString() + "00"]) / dataset.maxCapacity);
+    console.log(`${location} at ${capacity} of capacity`)
+    return capacity;
 
   }
 }
@@ -24,6 +41,10 @@ export default function CapacityScreen() {
       <Text>This is Capacity screen</Text>
       <Text>Gym Capacity</Text>
       <Progress.Bar progress={getProgress('gym')} />
+      <Text>Cafeteria Capacity</Text>
+      <Progress.Bar progress={getProgress('caf')} />
+      <Text>Parking Lot Capacity</Text>
+      <Progress.Bar progress={getProgress('lot')} />
     </View>
 
   );
