e186d16224b236fbdc574b5230837d06f634a1f7
Thomas Braccia <bracciata@gmail.com>
2021-07-23 09:35:08 -0500

    Temporarily removed progress bars but did add collapsibles.

--- screens/capacity/CapacityScreen.jsx
+++ screens/capacity/CapacityScreen.jsx
@@ -5,6 +5,7 @@ const gym = require('./data/gymTimes.json');
 const caf = require('./data/cafTimes.json');
 const lot = require('./data/lotTimes.json');
 import { Dimensions } from "react-native";
+import CollapsibleView from "@eliav2/react-native-collapsible-view";
 
 const screenWidth = Dimensions.get("window").width;
 
@@ -35,6 +36,7 @@ function getProgress(location) {
     return capacity;
   } else {
     let capacity = ((dataset.peopleAtTimes[0].weekends[time.getHours()][time.getHours().toString() + "00"]) / dataset.maxCapacity);
+    console.log('Capacity is: ' + capacity);
     return capacity;
 
   }
@@ -98,22 +100,14 @@ export default function CapacityScreen() {
     backgroundGradientFrom: "#fb8c00",
     backgroundGradientTo: "#ffa726",
     backgroundGradientToOpacity: .6,
-    fillShadowGradient: 'black',
+    fillShadowGradient: '#ffffff',
     fillShadowGradientOpacity: 1,
     decimalPlaces: 0, // optional, defaults to 2dp
     color: (opacity = 1) => `rgba(0, 0, 0, 1)`,
     strokeWidth: 1, // optional, default 3
     barPercentage: .8,
     useShadowColorFromDataset: false,// optional
-    scales: {
-      yAxes: [{
-        ticks: {
-          beginAtZero: true,
-          min: 0,
-          max: 100
-        }
-      }]
-    }
+
 
   };
   const styles = StyleSheet.create({
@@ -133,54 +127,52 @@ export default function CapacityScreen() {
     <SafeAreaView style={styles.container}>
       <ScrollView style={styles.scrollView}>
         <Text>This is Capacity screen</Text>
-        <Text>Gym Capacity</Text>
-        <Progress.Bar progress={getProgress('gym')} width={screenWidth - 10}
-          color={'rgba(255, 167, 76, 1)'} borderColor={'black'} height={20} borderWidth={2} />
-        <BarChart
-          style={{
-            marginVertical: 8,
-          }}
-          data={generateDataForBarChart('gym')}
-          width={screenWidth}
-          height={220}
-          yAxisSuffix="%"
-          fromZero={true}
+        <CollapsibleView title="Gym Capacity">
+          <BarChart
+            style={{
+              marginVertical: 8,
+            }}
+            data={generateDataForBarChart('gym')}
+            width={screenWidth}
+            height={220}
+            yAxisSuffix="%"
+            fromZero={true}
 
-          chartConfig={chartConfig}
-          verticalLabelRotation={30}
-        />
-        <Text>Cafeteria Capacity</Text>
-        <Progress.Bar progress={getProgress('caf')} width={screenWidth - 10}
-          color={'rgba(255, 167, 76, 1)'} borderColor={'black'} height={20} borderWidth={2} />
-        <BarChart
-          style={{
-            marginVertical: 8,
-          }}
-          data={generateDataForBarChart('caf')}
-          width={screenWidth}
-          height={220}
-          yAxisSuffix="%"
-          fromZero={true}
+            chartConfig={chartConfig}
+            verticalLabelRotation={30}
+          />
+        </CollapsibleView>
+        <CollapsibleView title="Cafeteria Capacity">
+          <BarChart
+            style={{
+              marginVertical: 8,
+            }}
+            data={generateDataForBarChart('caf')}
+            width={screenWidth}
+            height={220}
+            yAxisSuffix="%"
+            fromZero={true}
 
-          chartConfig={chartConfig}
-          verticalLabelRotation={30}
-        />
-        <Text>Parking Lot Capacity</Text>
-        <Progress.Bar progress={getProgress('lot')} width={screenWidth - 10}
-          color={'rgba(255, 167, 76, 1)'} borderColor={'black'} height={20} borderWidth={2} />
-        <BarChart
-          style={{
-            marginVertical: 8,
-          }}
-          data={generateDataForBarChart('lot')}
-          width={screenWidth}
-          height={220}
-          yAxisSuffix="%"
-          fromZero={true}
+            chartConfig={chartConfig}
+            verticalLabelRotation={30}
+          />
+        </CollapsibleView>
+        <CollapsibleView title="Parking Lot Capacity">
+          <BarChart
+            style={{
+              marginVertical: 8,
+            }}
+            data={generateDataForBarChart('lot')}
+            width={screenWidth}
+            height={220}
+            yAxisSuffix="%"
+            fromZero={true}
 
-          chartConfig={chartConfig}
-          verticalLabelRotation={30}
-        />
+            chartConfig={chartConfig}
+            verticalLabelRotation={30}
+          />
+          <Text>The above is the number of parking spots filled</Text>
+        </CollapsibleView>
       </ScrollView>
     </SafeAreaView>
   );
