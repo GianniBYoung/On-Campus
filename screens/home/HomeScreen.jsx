f842f495529f102fe754f3e5801b53e9b1c4f5c2
Thomas Braccia <bracciata@gmail.com>
2021-07-23 11:44:53 -0500

    Sophia's Home Page
    Co-Authored-By: sophia-chaltas <87824395+sophia-chaltas@users.noreply.github.com>

--- screens/home/HomeScreen.jsx
+++ screens/home/HomeScreen.jsx
@@ -1,22 +1,69 @@
 import React from "react";
-import { StyleSheet, Text, View, Button } from "react-native";
-
+import { StyleSheet, Text, View, SafeAreaView, Button, TouchableOpacity } from "react-native";
 export default function HomeScreen({ navigation }) {
   return (
-    <View>
-      <Text>This is Home screen</Text>
-      <Button
-        title="Capacity"
-        onPress={() => {
-          navigation.push("Capacity");
-        }}
-      ></Button>
-      <Button
-        title="Security"
-        onPress={() => {
-          navigation.push("Security");
-        }}
-      ></Button>
+    <View style={styles.container}​>
+      <Text style={styles.title}​>On Campus</Text>
+      <View style={styles.buttonContainer}​>
+        <TouchableOpacity
+          style={styles.securityButton}
+          onPress={() => { navigation.push("Security") }}​>
+          <Text style={styles.securityText}​>Security</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={styles.capacityButton}
+          onPress={() => { navigation.push("Capacity") }}​>
+          <Text style={styles.capacityText}​>Capacity</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
+const styles = StyleSheet.create({
+  container: {
+    width: "100%",
+    height: "100%",
+    alignItems: "center",
+    flexDirection: "column",
+    backgroundColor: "#337ab7"
+  },
+  title: {
+    fontSize: 30,
+    margin: 30,
+    marginTop: 60,
+    color: "white",
+    fontWeight: "bold",
+    justifyContent: "flex-start",
+  },
+  buttonContainer: {
+    width: "100%",
+    elevation: 30,
+    alignItems: "center",
+  },
+  securityText: {
+    fontSize: 18,
+    color: "red",
+  },
+  securityButton: {
+    width: 225,
+    height: 100,
+    marginBottom: 30,
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "white",
+    borderRadius: 10,
+  },
+  capacityText: {
+    fontSize: 18,
+    color: "black",
+  },
+  capacityButton: {
+    width: 225,
+    height: 100,
+    marginBottom: 30,
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "white",
+    borderRadius: 10,
+  },
+});
